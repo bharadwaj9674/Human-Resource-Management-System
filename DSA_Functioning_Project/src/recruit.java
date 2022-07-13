761cb39ec45bf82ca54b55951c2967796654b128
Bharadwaj Madiraju <91402980+bharadwaj9674@users.noreply.github.com>
2022-07-13 22:13:43 +0530

    Add files via upload

--- DSA_Functioning_Project/src/recruit.java
+++ DSA_Functioning_Project/src/recruit.java
@@ -1,60 +1,93 @@
 import java.util.*;
-class recruit 
-{
+import java.io.*;
+
+public class recruit{
+
     static Scanner sc=new Scanner(System.in);
     static int age, salary, tier;
+    static String name;
+    static node nodeage;
+    static node nodetier;
+    static node nodesalary;
     class node
     {
         int value;
-        node next;
+        node next,prev;
         node(int value)
         {
             this.value= value;
             next=null;
+            prev=null;
         }
     }
-    static node nodeage, nodetier, nodesalary;
-    void accept()
+    class qnode
+    {
+        String qvalue;
+        qnode qnext;
+        qnode(String value)
+        {
+            qvalue=value;
+            qnext=null;
+        }
+    }
+    void add(int a,node previous)
+    {
+        node newnode=new node(a);
+        previous.next=newnode;
+    }
+    
+    void accept() throws IOException
     {
         System.out.println("Enter name");
-        String name=sc.next();
+        name=sc.next();
         System.out.println("Enter age");
         age=sc.nextInt();
         System.out.println("Enter tier of college");
         tier=sc.nextInt();
         System.out.println("Enter excpected salary");
         salary=sc.nextInt();
-    }
-    void create()
-    {
         nodeage=new node(age);
         nodetier=new node(tier);
         nodesalary=new node(salary);
+    }
+    void create() throws IOException
+    {
         nodeage.next=nodetier;
         nodetier.next=nodesalary;
+        traversal();
     }
+
     boolean checkage(int age)
     {
-        if(age>45 && age<22)
-        return false;
-        else
-        return true;
+        if(age<45 && age>22){
+        System.out.println("true");
+            return true;
+        }
+        else{
+            return false;
     }
+}
+
     boolean checktier(int tier)
     {
-        if(tier>2&&tier<0)
-        return false;
+        if(tier<2&&tier>0)
+            return true;
         else
-        return true;
+            return false;
     }
+
     boolean checksalary(int salary)
     {
-        if(salary>100000&&salary<0)
-        return false;
-        else
-        return true;
+        if(salary<100000&&salary>0){
+            System.out.println("true");
+            return true;
+        }
+        else{
+            return false;
+        }
     }
-    void traversal()
+
+    void traversal() throws IOException
     {
         if((checkage(nodeage.value)))
         {
@@ -63,31 +96,38 @@ class recruit
                 if((checksalary(nodesalary.value)))
                 {
                     System.out.println("the candidate is selected");
+                    FileWriter fw=new FileWriter("/Users/bharath/Desktop/DSA_Project/recruit.txt",true);
+                    System.out.println(name+","+age+","+tier+","+salary);
+                    fw.write(name+","+age+","+tier+","+salary);
+                    fw.write(System.lineSeparator());
+                    fw.close();
                 }
                 else
-                System.out.println("candidate rejected");
+                    System.out.println("candidate rejected");
             }
             else
                 System.out.println("candidate rejected");
         }
-        else
-                System.out.println("candidate rejected");
+        else{
+            System.out.println("candidate rejected");
     }
-    void list()
+}
+    void number() throws IOException
     {
         System.out.println("Enter the number of candidates");
         int n=sc.nextInt();
-        while(n>0)
+        for(int i=0;i<n;i++)
         {
             accept();
-            create();
-            traversal();
-            n--;
         }
     }
-    public static void main(String args[])
+    
+
+    public static void main(String args[]) throws IOException
     {
         recruit ob=new recruit();
-        ob.list();
+        ob.number();
+        ob.create();
+        ob.traversal();
     }
 }
